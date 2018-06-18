1950fa2597b3b6273e52b78bc79ba3fe8e260add
Ryan Challen <ryanchallen@outlook.com>
2018-06-18 12:39:53 +1000

    Update logic and interactions
    Added:
    - "winner" color class to green
    - change the "playing to" value
    - reset game button functionality
    - refactor reset to a function
    May refactor further later as code isn't DRY.

--- scorekeeper.js
+++ scorekeeper.js
@@ -3,15 +3,28 @@ var p2Button = document.querySelector("#p2");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var resetButton = document.querySelector("#reset");
+var numImput = document.querySelector("input");
+var p = document.querySelector("p");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
 var winningScore = 5;
 
+function reset(){
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
+}
+
 p1Button.addEventListener("click", function(){
   if(!gameOver){
     p1Score++;
     if(p1Score === winningScore){
+      p1Display.classList.add("winner");
       gameOver = true;
     }
     p1Display.textContent = p1Score;
@@ -22,8 +35,19 @@ p2Button.addEventListener("click", function(){
   if(!gameOver){
     p2Score++;
     if(p2Score === winningScore){
+      p2Display.classList.add("winner");
       gameOver = true;
     }
     p2Display.textContent = p2Score;
   }
+});
+
+resetButton.addEventListener("click", function(){
+  reset();
+});
+
+numImput.addEventListener("change", function(){
+  playTo.textContent = this.value;
+  winningScore = Number(this.value);
+  reset();
 });
